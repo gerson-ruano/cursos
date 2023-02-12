7043778078038fdf2d042f25c157cff64d643fe8
gersonruano <toge619@gmail.com>
2023-02-12 16:14:50 -0600

    primer commit de sitio cursos en laravel

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Course;
+use App\Models\Post;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::factory(5)->create();
+        Category::factory(3)->create();
+        Course::factory(10)->create();
+        Post::factory(90)->create();
+    }
+}
