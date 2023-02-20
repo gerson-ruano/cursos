e4d548a8bccbd4ce011bb7f2e311e5953ac148af
gersonruano <toge619@gmail.com>
2023-02-19 21:19:21 -0600

    plantilla principal, login de registro, primer componente livewire

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.web')
+
+@section('content')
+
+    <div class="text-center">
+        <h1 class="text-3xl text-gray-700 mb-2 uppercase">Ultimos Curso</h1>
+        <h2 class="text-xl text-gray-600 ">Formate online como profesional en tecnologia</h2>
+        <h3 class="text-lg text-gray-600">70% de los graduados duplica sus ingresos </h3>
+    </div>
+
+    <livewire:course-list>
+
+@endsection
